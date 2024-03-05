d13d469064fd6a5f4be29aebebfc22f697dbfa18
jram828 <128335805+jram828@users.noreply.github.com>
2024-03-05 14:19:30 -0500

    M4 - SQL

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -4,11 +4,11 @@ const ejercicio02 = "SELECT * FROM movies WHERE duration<90;";
 
 const ejercicio03 = "SELECT * FROM movies WHERE year>=1930 AND year<=1940;";
 
-const ejercicio04 = " SELECT * FROM movies WHERE title LIKE " % til % "";
+const ejercicio04 = " SELECT * FROM movies WHERE title LIKE " % til % ";";
 
 const ejercicio05 = "SELECT * FROM movies WHERE array_length(actors, 1) = 1;";
 
-const ejercicio06 = '';
+const ejercicio06 = "SELECT title, AVG(rating) FROM movies,unnest(ratings) rating GROUP BY title;";
 
 const ejercicio07 = "SELECT actors FROM movies WHERE title LIKE "%Fast_and%" AND year=2016;";
 
